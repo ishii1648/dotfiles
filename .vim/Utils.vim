9d87d2f507d90e4e7b6ed5d7a3b43eefc50ba0b0
Sho Ishii <ishii1492@gmail.com>
2025-12-31 01:03:24 +0900

    feat: delete unused configs

--- .vim/Utils.vim
+++
@@ -1,8 +0,0 @@
-" 自動実行用SCPの登録
-function! SyncFile(local_dir, remote_dir)
-    let upload_settings = { a:local_dir: a:remote_dir }
-    let cnt_dir = expand("%:p:h")
-    if has_key(upload_settings, cnt_dir)
-        exe '!scp % ' .  a:remote_dir
-    endif
-endfunction
--- .zshrc.d/alias.zsh
+++
@@ -1,8 +0,0 @@
-alias s='ssh $(grep -iE "^host[[:space:]]+[^*]" ~/.ssh/config|peco|awk "{print \$2}")'
-alias ls='ls --color=auto'
-alias la='ls -la --color=auto'
-alias ll='ls -l --color=auto'
-alias rm='rm -i'
-alias fc="find_cd"
-alias g='git'
-alias wp='docker run -it --rm --volumes-from wordpress --network container:wordpress wordpress:cli'
