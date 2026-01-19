d7145dc12e803b1972edc3f2c86fa1e41ccaf9a3
sho ishii <sho-ishii@c-fo.com>
2026-01-19 20:59:37 +0900

    feat: fish shellにzoxide初期化を追加
    - zoxide init fish | source を追加してzoxideを有効化
    - 不要なGPG関連のコメントアウトされたコードを削除

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,5 @@ if status is-interactive
     # conf.d/ の設定ファイルが自動的に読み込まれます
 end
 
-#set -x GPG_TTY (tty)
-#if not pgrep -x gpg-agent > /dev/null
-#    gpg-agent --daemon --enable-ssh-support
-#end
+zoxide init fish | source
 
