42f24558df20e60451a3445ef7ec97f43e8b1211
Sho Ishii <ishii1492@gmail.com>
2026-03-01 02:05:16 +0900

    fix: aqua/zoxide 未インストール時の fish 起動エラーを防止
    - env.fish: aqua コマンドが無い場合にデフォルトの AQUA_ROOT_DIR をフォールバック
    - config.fish: zoxide が無い場合のパースエラーをガード

--- configs/fish/conf.d/env.fish
+++ configs/fish/conf.d/env.fish
@@ -10,6 +10,10 @@ set -gx KUBE_EDITOR vim
 
 # Package managers
 set -gx AQUA_PROGRESS_BAR true
-set -gx AQUA_ROOT_DIR (aqua root-dir)
+if command -q aqua
+    set -gx AQUA_ROOT_DIR (aqua root-dir)
+else
+    set -gx AQUA_ROOT_DIR $HOME/.local/share/aquaproj-aqua
+end
 set -gx NPM_CONFIG_PREFIX $HOME/.local/share/npm-global
 
--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     # conf.d/ の設定ファイルが自動的に読み込まれます
     stty -ixon
-    zoxide init fish | source
+    command -q zoxide && zoxide init fish | source
 end
