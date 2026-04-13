fddd5fa9f9719ce26c282792871c8be7021dab58
sho ishii <sho-ishii@c-fo.com>
2026-04-14 00:17:31 +0900

    feat: PRs タブのヘッダーにセッション状態の凡例を追加
    ● open (黄) = tmux セッション有、● no session (dim) = セッションなし

--- configs/fish/functions/__dl_fzf_toggle.fish
+++ configs/fish/functions/__dl_fzf_toggle.fish
@@ -2,16 +2,19 @@ function __dl_fzf_toggle --description 'dispatch_launcher fzf タブトグル（
     set -l mode_file /tmp/dl-fzf-pr-mode
     set -l bold_green (printf '\e[1;92m')
     set -l bold_cyan (printf '\e[1;96m')
+    set -l yellow (printf '\e[33m')
     set -l dim (printf '\e[90m')
     set -l reset (printf '\e[0m')
 
+    set -l tab_hint (printf '%stab: switch  ' $dim)
+    set -l repos_active (printf '%srepos%s / %sPRs%s' $bold_green $reset $dim $reset)
+    set -l prs_active (printf '%srepos%s / %sPRs%s    %s●%s open  %s●%s no session' $dim $reset $bold_cyan $reset $yellow $dim $dim $reset)
+
     if test -f $mode_file
         rm $mode_file
-        set -l header (printf '  %stab: switch  %srepos%s / %sPRs%s' $dim $bold_green $reset $dim $reset)
-        echo "reload(fish -c __dl_repo_candidates)+change-prompt(> )+change-header($header)"
+        echo "reload(fish -c __dl_repo_candidates)+change-prompt(> )+change-header(  $tab_hint$repos_active)"
     else
         touch $mode_file
-        set -l header (printf '  %stab: switch  %srepos%s / %sPRs%s' $dim $dim $reset $bold_cyan $reset)
-        echo "reload(fish -c __dl_pr_worktree_candidates)+change-prompt(> )+change-header($header)"
+        echo "reload(fish -c __dl_pr_worktree_candidates)+change-prompt(> )+change-header(  $tab_hint$prs_active)"
     end
 end
